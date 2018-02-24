d0936552757e03d36aa68d2f14822b1cdde0a12d
Jonas Andersen <jonas@anrsn.me>
2018-02-24 14:27:40 +0100

    Added tests for compile & export and moved start to the end (GUI)

--- test/NAVDevEnv.Tests.ps1
+++ test/NAVDevEnv.Tests.ps1
@@ -13,16 +13,18 @@ describe 'NAVDevEnv module tests' {
       }
     }
     function Get-TempCodeunitObject {
-      Param($FileName)
+      Param($FileName, $ObjectDatabase)
       $ID = Get-Random -Minimum 50000 -Maximum 100000
       $Name = $(New-Guid).Guid.Replace("-", "").Substring(0, 20)
-      $object = "OBJECT Codeunit $ID $Name`n {`n  OBJECT-PROPERTIES`n {`n    Date=; `n    Time=; `n    Version List=; `n  }`n  PROPERTIES`n {`n    OnRun=BEGIN`n            MESSAGE('hello worldæøå'); `n          END; `n`n  }`n  CODE`n {`n`n    BEGIN`n    END.`n  }`n}`n`n"
+      $object = "OBJECT Codeunit $ID $Name`n {`n  OBJECT-PROPERTIES`n {`n    Date=; `n    Time=; `n    Version List=; `n  }`n  PROPERTIES`n {`n    OnRun=BEGIN`n            MESSAGE('hello world'); `n          END; `n`n  }`n  CODE`n {`n`n    BEGIN`n    END.`n  }`n}`n`n"
       $FilterText = "ID={0};Type=5" -f $ID
+      $Query = "SELECT * FROM [{0}].dbo.[Object] WHERE [ID] = {1} AND [Type] = 5" -f $ObjectDatabase, $ID
       $object | Out-File $FileName -Encoding oem
       [PSCustomObject]@{
         ID         = $ID
         Name       = $Name
         Object     = $Object
+        Query      = $Query
         File       = Resolve-Path $FileName
         FilterText = $FilterText
       }
@@ -31,12 +33,6 @@ describe 'NAVDevEnv module tests' {
 
   AfterAll {
     Get-Process -Name "finsql" -ErrorAction Ignore | Stop-Process -ErrorAction Ignore
-  }
-
-  it "Start NAVIde" {
-    Start-NAVIde -DatabaseName $Database
-    $result = Get-Process -Name "finsql"
-    $result | Should Not BeNullOrEmpty
   }
 
   it "Can create nav databases" {
@@ -51,17 +47,58 @@ describe 'NAVDevEnv module tests' {
   }
 
   it "Can import/remove nav object" {
-    $importobject = Get-TempCodeunitObject -FileName "importobject.txt"
+    $importobject = Get-TempCodeunitObject -FileName "importobject.txt" -ObjectDatabase $Database
     try {
-      $SelectStmt = "SELECT * FROM [{0}].dbo.[Object] WHERE [ID] = {1} AND [Type] = 5" -f $Database,$importobject.ID
-      Invoke-Sqlcmd -Query $SelectStmt | Should BeNullOrEmpty
+      Invoke-Sqlcmd -Query $importobject.Query | Should BeNullOrEmpty
       Import-NAVApplicationObject -DatabaseName $Database -Path $importobject.File -ImportAction Overwrite -SynchronizeSchemaChanges No
-      Invoke-Sqlcmd -Query $SelectStmt | Should Not BeNullOrEmpty
+      Invoke-Sqlcmd -Query $importobject.Query | Should Not BeNullOrEmpty
       Remove-NAVApplicationObject -DatabaseName $Database -Filter $importobject.FilterText -SynchronizeSchemaChanges No -Confirm:$false
-      Invoke-Sqlcmd -Query $SelectStmt | Should BeNullOrEmpty
+      Invoke-Sqlcmd -Query $importobject.Query | Should BeNullOrEmpty
     }
     finally {
       Remove-Item $importobject.File -ErrorAction Ignore
     }
+  }
+
+  it "Can compile nav object" {
+    $importobject = Get-TempCodeunitObject -FileName "compileobject.txt" -ObjectDatabase $Database
+    try {
+      Invoke-Sqlcmd -Query $importobject.Query | Should BeNullOrEmpty
+      Import-NAVApplicationObject -DatabaseName $Database -Path $importobject.File -ImportAction Overwrite -SynchronizeSchemaChanges No
+      $beforeCompile = Invoke-Sqlcmd -Query $importobject.Query
+      $beforeCompile.Compiled | Should be $false
+      Start-CompileNAVApplicationObject -DatabaseName $Database -Filter $importobject.FilterText -SynchronizeSchemaChanges No
+      $afterCompile = Invoke-Sqlcmd -Query $importobject.Query
+      $afterCompile.Compiled | Should be $true
+    }
+    finally {
+      Remove-NAVApplicationObject -DatabaseName $Database -Filter $importobject.FilterText -SynchronizeSchemaChanges No -Confirm:$false -ErrorAction Ignore
+      Remove-Item $importobject.File -ErrorAction Ignore
+    }
+  }
+
+  it "Can export nav object" {
+    $importobject = Get-TempCodeunitObject -FileName "exportobject.txt" -ObjectDatabase $Database
+    $exportfile = "exportobject2.txt"
+    try {
+      Remove-Item $exportfile -ErrorAction Ignore
+      Invoke-Sqlcmd -Query $importobject.Query | Should BeNullOrEmpty
+      Import-NAVApplicationObject -DatabaseName $Database -Path $importobject.File -ImportAction Overwrite -SynchronizeSchemaChanges No
+      Invoke-Sqlcmd -Query $importobject.Query | Should Not BeNullOrEmpty
+      Test-Path $exportfile | Should Be $false
+      Export-NAVApplicationObject -DatabaseName $Database -Filter $importobject.FilterText -Path $exportfile
+      Test-Path $exportfile | Should Be $true
+    }
+    finally {
+      Remove-NAVApplicationObject -DatabaseName $Database -Filter $importobject.FilterText -SynchronizeSchemaChanges No -Confirm:$false -ErrorAction Ignore
+      Remove-Item $importobject.File -ErrorAction Ignore
+      Remove-Item $exportfile -ErrorAction Ignore
+    }
+  }
+
+  it "Start NAVIde" {
+    Start-NAVIde -DatabaseName $Database
+    $result = Get-Process -Name "finsql"
+    $result | Should Not BeNullOrEmpty
   }
 }
